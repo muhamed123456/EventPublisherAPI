76386e2fd96a12b57c3029e9465ba7f8a47be3bb
muhammedamzovski96 <muhammedamzovski96@gmail.com>
2018-04-15 23:32:13 +0200

    Created the Entity Data Models. Created Web API 2 Controller with actions using Entity Framework - PublishersController.

---
+++ EventPublisherAPI/Controllers/PublishersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using EventPublisherAPI.Models;
+
+namespace EventPublisherAPI.Controllers
+{
+    public class PublishersController : ApiController
+    {
+        private EventPublisherDBEntities db = new EventPublisherDBEntities();
+
+        // GET: api/Publishers
+        public IQueryable<Publisher> GetPublishers()
+        {
+            return db.Publishers;
+        }
+
+        // GET: api/Publishers/5
+        [ResponseType(typeof(Publisher))]
+        public IHttpActionResult GetPublisher(int id)
+        {
+            Publisher publisher = db.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(publisher);
+        }
+
+        // PUT: api/Publishers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPublisher(int id, Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != publisher.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(publisher).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PublisherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Publishers
+        [ResponseType(typeof(Publisher))]
+        public IHttpActionResult PostPublisher(Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Publishers.Add(publisher);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = publisher.ID }, publisher);
+        }
+
+        // DELETE: api/Publishers/5
+        [ResponseType(typeof(Publisher))]
+        public IHttpActionResult DeletePublisher(int id)
+        {
+            Publisher publisher = db.Publishers.Find(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            db.Publishers.Remove(publisher);
+            db.SaveChanges();
+
+            return Ok(publisher);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return db.Publishers.Count(e => e.ID == id) > 0;
+        }
+    }
+}
